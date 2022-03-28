6559aad954d13d18940679ab7c8effa8bb5ef21c
Roshan Rane <roshan.rane97@gmail.com>
2022-03-28 21:20:10 +0530

    - Added menu for subcategories - Added Navigation links for categories & subcategories

--- src/components/commons/CategoryCard.js
+++ src/components/commons/CategoryCard.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Paper from "@mui/material/Paper";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import CardActions from "@mui/material/CardActions";
-import Box from "@mui/material/Box";
 import {KeyboardArrowDown} from "@mui/icons-material";
 import {Button} from "@mui/material";
 import Stack from "@mui/material/Stack";
+import {useNavigate} from "react-router-dom";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const CategoryCard = (props) => {
+    // Navigation Hooks
+    const navigate = useNavigate();
+
+    // Menu States
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <>
             <Paper
@@ -19,6 +32,7 @@ const CategoryCard = (props) => {
             >
                 <Stack
                     direction="row"
+                    onClick={() => navigate(`/search?category=${props.category.categoryName.toLowerCase()}`)}
                     sx={{
                         p: 1,
                         justifyContent: "center",
@@ -33,24 +47,49 @@ const CategoryCard = (props) => {
                     />
                 </Stack>
                 <Button
-                    endIcon={<KeyboardArrowDown
-                        sx={{
-                            ml: -1
-                        }}
-                    />}
+                    id="subcategory-button"
+                    aria-haspopup="true"
+                    aria-controls={open ? 'subcategory-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    endIcon={
+                        props.category.subCategory.length > 0 &&
+                        <KeyboardArrowDown sx={{ml: -1}}/>
+                    }
                 >
                     <Typography
                         align="center"
                         color="black"
                         variant="subtitle2"
-                        sx={{
-                            textTransform: "capitalize"
-                        }}
+                        sx={{textTransform: "capitalize"}}
                     >
                         {props.category.categoryName}
                     </Typography>
                 </Button>
             </Paper>
+            <Menu
+                id="subcategory-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'subcategory-button',
+                }}
+            >
+                {
+                    props.category.subCategory.length > 0 &&
+                    props.category.subCategory.map((subCategory, idx) => {
+                        return <MenuItem
+                            key={idx}
+                            onClick={() => {
+                                navigate(`/search?category=${props.category.categoryName.toLowerCase()}`
+                                    + `&subcategory=${subCategory.toLowerCase()}`)
+                            }}>
+                            {subCategory}
+                        </MenuItem>
+                    })
+                }
+            </Menu>
         </>
     );
 };
