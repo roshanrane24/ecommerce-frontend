6eed3808e9ebb0c108928d25dcd9839e4c36d289
Roshan Rane <roshan.rane97@gmail.com>
2022-03-28 21:17:43 +0530

    Delegated Result Handling to Caller

--- src/api/CategoryService.js
+++ src/api/CategoryService.js
@@ -1,86 +1,10 @@
 import client from './HttpClient';
 
 class CategoryService {
+    // Get list of categories with all subcategories
     getCategoryList() {
         return client.get("/categories/all")
-            .then(response => response.data)
-            .catch(error => {
-                return [
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                    {
-                        categoryName: "Grocery",
-                        imageName: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-                        subCategory: [
-                            "a",
-                            "b",
-                            "c"
-                        ]
-                    },
-                ]
-            });
-
+            .then(response => response.data);
     }
 }
 
