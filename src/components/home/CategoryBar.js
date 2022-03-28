84039b0873a2daee2023f202f6fe2d9e8b6492fa
Roshan Rane <roshan.rane97@gmail.com>
2022-03-28 21:22:54 +0530

    - Added handling for service method result for get categories - Added Error message with ability to reload component when error occurs during request

--- src/components/home/CategoryBar.js
+++ src/components/home/CategoryBar.js
@@ -1,21 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import CategoryService from "../../api/CategoryService";
-import Grid from "@mui/material/Grid";
 import CategoryCard from "../commons/CategoryCard";
-import {Paper} from "@mui/material";
 import Stack from "@mui/material/Stack";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 const CategoryBar = () => {
 
     // Render Categories
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
 
-    useEffect(() => {
+    const getCategories = () => {
         CategoryService.getCategoryList()
-            .then(response => setCategories(response))
-            .catch(response => setCategories(response))
-    }, []);
+            .then(response => {
+                setCategories(response);
+                setErrorMessage(null);
+            })
+            .catch(error => {
+                setCategories(null);
+                setErrorMessage(error.message);
+            })
+    }
 
+    // Init
+    useEffect(() => getCategories(), []);
 
     return (
         <Stack
@@ -23,14 +32,19 @@ const CategoryBar = () => {
             spacing={2}
             justifyContent="space-around"
             alignItems="center"
-            sx={{
-                maxWidth: "100%",
-            }}
+            sx={{maxWidth: "100%"}}
         >
             {
+                categories &&
                 categories.map((category, idx) => <CategoryCard key={idx} category={category}/>)
             }
-
+            {
+                errorMessage &&
+                <Alert severity="error" sx={{m: 2, flexGrow: 1}}>
+                    <AlertTitle>Error</AlertTitle>
+                    {errorMessage} — <strong onClick={() => getCategories()}>Reload</strong>
+                </Alert>
+            }
         </Stack>
     );
 };
