799d880dfcccd4ee89d1518fd23c0055505242a2
SawantVinit22 <sawantvinit22@gmail.com>
2022-03-30 14:54:09 +0530

    added api method to getproductdetails

--- src/api/ProductService.js
+++ src/api/ProductService.js
@@ -5,6 +5,11 @@ class ProductService {
         return client.get("/products/latest")
             .then(response => response.data);
     }
+
+    getProductDetails = (product_id) => {
+        return client.get(`/products/${product_id}`).
+            then(response => response.data);
+    }
 }
 
 export default new ProductService();
