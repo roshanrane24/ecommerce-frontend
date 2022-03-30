5ae468a261a9696de7588c33ed2d61e005f36ac2
SawantVinit22 <sawantvinit22@gmail.com>
2022-03-30 14:53:31 +0530

    product page route added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login.js';
 import Register from './components/register/Register';
 import Home from './components/home/Home';
@@ -8,8 +8,9 @@ import UserProvider from "./Context/UserContext";
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import {createTheme} from "@mui/material";
-import {grey} from "@mui/material/colors";
+import { createTheme } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import ProductPage from './components/Product/ProductPage.js';
 
 const theme = createTheme({
     palette: {
@@ -22,15 +23,16 @@ const theme = createTheme({
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline/>
+            <CssBaseline />
             <UserProvider>
                 <Routes>
-                    <Route path='/' element={<Header/>}>
-                        <Route index element={<Home/>}/>
-                        <Route path='*' element={<NotFound/>}/>
+                    <Route path='/' element={<Header />}>
+                        <Route index element={<Home />} />
+                        <Route path='/product/:product_id' element={<ProductPage />} />
+                        <Route path='*' element={<NotFound />} />
                     </Route>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/register' element={<Register/>}/>
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
                 </Routes>
             </UserProvider>
         </ThemeProvider>
