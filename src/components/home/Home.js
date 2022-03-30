61795f692c37e88268577fd90b5c2f76dc10315d
Roshan Rane <roshan.rane97@gmail.com>
2022-03-31 00:27:41 +0530

    Added Mostly Viewed product

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,6 +4,7 @@ import NewProducts from "./NewProducts";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+import MostViewedProducts from "./MostViewedProducts";
 
 const Home = () => {
     return (
@@ -16,6 +17,11 @@ const Home = () => {
             <Stack sx={{paddingX: 2, flexGrow: 1}}>
                 <Paper elevation={1} sx={{p: 2}}>
                     <NewProducts/>
+                </Paper>
+            </Stack>
+            <Stack sx={{paddingX: 2, flexGrow: 1}}>
+                <Paper elevation={1} sx={{p: 2}}>
+                    <MostViewedProducts/>
                 </Paper>
             </Stack>
         </Stack>
