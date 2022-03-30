19c1b8ed7f91afe78c7d1b3236afc5d774fb1c0f
Roshan Rane <roshan.rane97@gmail.com>
2022-03-30 23:37:46 +0530

    No Cors Header

--- src/api/HttpClient.js
+++ src/api/HttpClient.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const client = axios.create({
     baseURL: 'http://localhost:8080/api',
-    timeout: 3000
+    timeout: 3000,
+    mode: 'no-cors'
 })
 export default client;
