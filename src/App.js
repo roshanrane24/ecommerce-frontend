2da8d3b53063098e4a58d0a9f8a1bb111b900d53
Roshan Rane <roshan.rane97@gmail.com>
2022-03-31 15:29:40 +0530

    - Changes from new code (Heroku)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import {createTheme} from "@mui/material";
 import {grey} from "@mui/material/colors";
+import ProductPage from "./components/Product/ProductPage";
 
 const theme = createTheme({
     palette: {
@@ -29,6 +30,7 @@ function App() {
                     <Routes>
                         <Route path='/' element={<Header/>}>
                             <Route index element={<Home/>}/>
+                            <Route path='product/:product_id' element={<ProductPage/>}/>
                             <Route path='*' element={<NotFound/>}/>
                         </Route>
                         <Route path='/login' element={<Login/>}/>
--- src/components/Product/ProductPage.js
+++ src/components/Product/ProductPage.js
@@ -1,44 +1,65 @@
-import { Box, Typography, Button, Stack, Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Paper } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+    Box,
+    Button,
+    Paper,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import StarIcon from "@mui/icons-material/Star";
 import ProductService from "../../api/ProductService";
-import { useParams, useNavigate } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect, useState} from 'react';
+import client from "../../api/HttpClient";
 
 const ProductPage = (props) => {
-    const navigate = useNavigate();
-    const [productDetails, setproductDetails] = useState(null);
-
-    const params = useParams()
-    const getProductDetails = () => {
-        ProductService.getProductDetails(params.product_id).then((result) => {
-            setproductDetails(result)
-        })
-        console.log(params);
-
-    }
-    useEffect(() => {
-        getProductDetails();
-    }, []);
-    const themeButton = createTheme({
+    // Theme
+    const ThemeButton = createTheme({
         palette: {
             primary: {
                 main: "#b71c1c"
             }
         }
     });
-    const themeIncl = createTheme({
+
+    const ThemeIncl = createTheme({
         palette: {
             primary: {
                 main: "#9e9e9e"
             }
         }
     });
+
+    // Routing
+    const navigate = useNavigate();
+    const params = useParams()
+
+    // States
+    const [productDetails, setproductDetails] = useState(null);
+
+    // Product details handler
+    const getProductDetails = () => {
+        ProductService.getProductDetails(params.product_id).then((result) => {
+            setproductDetails(result)
+        })
+    }
+
+    // init
+    useEffect(() => {
+        getProductDetails();
+    }, []);
+
     return (
         <Stack
-            sx={{ p: "8" }}
+            sx={{p: "8"}}
             direction="row"
             justifyContent="space-evenly"
             spacing={2}
@@ -61,7 +82,7 @@ const ProductPage = (props) => {
                         alignItems="center"
                     >
                         <img
-                            src={`http://localhost:8080/api/products/image/${productDetails.id}`}
+                            src={`${client.defaults.baseURL}/products/image/${productDetails.id}`}
                             alt={productDetails.name}
                             width="auto"
                             height="auto"
@@ -83,36 +104,41 @@ const ProductPage = (props) => {
                         <Typography variant="h5" gutterBottom>
                             {productDetails.name}
                         </Typography>
-                        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                            {productDetails.price.toLocaleString('en-IN', { style: 'currency', currency: 'INR', maximumSignificantDigits: 3 })}
+                        <Typography variant="h6" sx={{fontWeight: "bold"}}>
+                            {productDetails.price.toLocaleString('en-IN', {
+                                style: 'currency',
+                                currency: 'INR',
+                                maximumSignificantDigits: 3
+                            })}
                         </Typography>
-                        <ThemeProvider theme={themeIncl}>
-                            <Typography gutterBottom sx={{ color: "primary.main" }}>
+                        <ThemeProvider theme={ThemeIncl}>
+                            <Typography gutterBottom sx={{color: "primary.main"}}>
                                 inclusive of all taxes
                             </Typography>
                         </ThemeProvider>
                         <Button
                             variant="contained"
-                            endIcon={<StarIcon />}
+                            endIcon={<StarIcon/>}
                             color="success"
                             size="small"
                             disableElevation
                             disableRipple
-                            sx={{ mb: "16px" }}
+                            sx={{mb: "16px"}}
                         >
                             {productDetails.rating}
                         </Button>
-                        <ThemeProvider theme={themeButton}>
+                        <ThemeProvider theme={ThemeButton}>
                             <Stack
                                 direction="row"
                                 spacing={2}
                                 display="block"
-                                sx={{ mb: "16px" }}
+                                sx={{mb: "16px"}}
                             >
-                                <Button variant="contained" endIcon={<ShoppingCartIcon />} onClick={() => navigate(`/checkout/${productDetails.id}`)}>
+                                <Button variant="contained" endIcon={<ShoppingCartIcon/>}
+                                        onClick={() => navigate(`/checkout/${productDetails.id}`)}>
                                     Buy Now
                                 </Button>
-                                <Button variant="outlined" endIcon={<AddShoppingCartIcon />}>
+                                <Button variant="outlined" endIcon={<AddShoppingCartIcon/>}>
                                     Add to Cart
                                 </Button>
                             </Stack>
