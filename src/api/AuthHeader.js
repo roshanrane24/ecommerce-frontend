43ea5c6a8665e7b31028c93a5c11ddac44d38839
Roshan Rane <roshan.rane97@gmail.com>
2022-04-02 02:36:13 +0530

    Changed to use auth token

--- src/api/AuthHeader.js
+++ src/api/AuthHeader.js
@@ -1,9 +1,12 @@
-export default function authHeader(userDetails) {
-    // Get user from store
-    const user = userDetails;
+import AuthService from "./AuthService";
 
-    if (user && user.accessToken) {
-        return {Authorization: 'Bearer ' + user.accessToken}; // for Spring Boot back-end
+export default function authHeader() {
+    // Get user from store
+    const user = AuthService.getUserDetails();
+
+    // check if user has a token
+    if (user && user.token) {
+        return {Authorization: 'Bearer ' + user.token}; // for Spring Boot back-end
     } else {
         return {};
     }
