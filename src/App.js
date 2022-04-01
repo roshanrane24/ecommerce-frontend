c3d4dbd47fc7627692bf40912cabf77e05337547
SawantVinit22 <sawantvinit22@gmail.com>
2022-04-01 23:24:09 +0530

    checkout page added in checkout branch

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import ErrorBoundary from "./components/commons/ErrorBoundary";
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import {createTheme} from "@mui/material";
-import {grey} from "@mui/material/colors";
+import { createTheme } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import ProductPage from "./components/Product/ProductPage";
+import Checkout from './components/Checkout/Checkout.js';
 
 const theme = createTheme({
     palette: {
@@ -28,13 +29,15 @@ function App() {
             <UserProvider>
                 <ErrorBoundary>
                     <Routes>
-                        <Route path='/' element={<Header/>}>
-                            <Route index element={<Home/>}/>
-                            <Route path='product/:product_id' element={<ProductPage/>}/>
-                            <Route path='*' element={<NotFound/>}/>
+                        <Route path='/' element={<Header />}>
+                            <Route index element={<Home />} />
+                            <Route path='product/:product_id' element={<ProductPage />} />
+                            <Route path='*' element={<NotFound />} />
                         </Route>
                         <Route path='/login' element={<Login />} />
                         <Route path='/signup' element={<SignUp />} />
+                        <Route path='/checkout' element={<Checkout />} />
+
                     </Routes>
                 </ErrorBoundary>
             </UserProvider>
