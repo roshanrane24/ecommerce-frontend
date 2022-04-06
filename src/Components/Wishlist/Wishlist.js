910823a1757adda0305fdb649def61113563c1c2
SawantVinit22 <sawantvinit22@gmail.com>
2022-04-06 23:09:16 +0530

    wishlist added prdouct list card image bug fixed

--- src/Components/Wishlist/Wishlist.js
+++ src/Components/Wishlist/Wishlist.js
@@ -1,6 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductListCard from "../Commons/ProductListCard";
 import WishListService from "../../api/WishListService";
+import { Stack, Box, Typography, Divider } from '@mui/material';
+
 
 const Wishlist = () => {
 
@@ -15,11 +17,45 @@ const Wishlist = () => {
             .catch(error => console.log(error))
     }, []);
     return (
-        <div>
-            {
-                cartItems.map(product => <ProductListCard product={product} wishlist/>)
-            }
-        </div>
+        <>
+            {/* <Stack sx={{
+                mt: 2,
+                justifyContent: 'center',
+
+            }}>
+                <Typography variant='h4' sx={{ textAlign: 'center' }} gutterBottom>Wishlist</Typography>
+                <Divider />
+            </Stack> */}
+
+            <Stack
+                sx={{
+                    alignItems: 'flex-start',
+                    justifyContent: 'center',
+                    px: 30,
+                    pt: 2,
+                    flexGrow: 1
+
+                }} spacing={5}
+                direction='row' >
+                <Stack spacing={2}
+                    sx={{
+                        alignItems: 'center',
+                        justifyContent: 'center', flexGrow: 1
+                    }}>
+                    {
+                        cartItems.length > 0 ? (cartItems.map((product) =>
+                            <Box width='100%' sx={{
+                                px: 2
+                            }}>
+                                <ProductListCard product={product} wishlist />
+                            </Box>
+                        )) : ("Wishlist is empty")
+
+                    }
+                </Stack>
+            </Stack >
+        </>
+
     );
 };
 
